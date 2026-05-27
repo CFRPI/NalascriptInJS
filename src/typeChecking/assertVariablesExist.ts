ecf976f663f9af87d4f4bd96beb02b0b1f3868d8
Connor F. Reed <46138604+Blank2275@users.noreply.github.com>
2026-05-27 15:29:25 -0600

    removed console.log

--- src/typeChecking/assertVariablesExist.ts
+++ src/typeChecking/assertVariablesExist.ts
@@ -24,7 +24,6 @@ function assertVariableExistWhenUsedHelper(declaration: Declaration, lineInBlock
 
 function assertVariableExistWhenUsedStmt(stmt: Statement, lineInBlock: number, scopeName: string, scopes: Map<string, Scope>) {
     const scope = scopes.get(scopeName)
-    console.log(scopeName)
     if (!scope) {
         throw new Error("Internal Error asseertVariableExistWhenUsedHelper accessed nonexistent scope")
     }
